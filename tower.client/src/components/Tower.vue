272e3a9a076d5b7378f2aa465b2abe77cd206b05
Katie McCauley <katiemccau02@gmail.com>
2022-03-19 01:57:12 -0600

    added router link

--- tower.client/src/components/Tower.vue
+++ tower.client/src/components/Tower.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="component">
-    <h3>{{ tower.name }}</h3>
-    <h3>
-      {{
-        new Date(tower.startDate || tower.event.startDate).toLocaleDateString()
-      }}
-    </h3>
+  <router-link :to="{ name: 'EventDetails', params: { id: tower.id } }">
+    <div class="component text-dark">
+      <h3>{{ tower.name }}</h3>
+      <h3>
+        {{
+          new Date(
+            tower.startDate || tower.event.startDate
+          ).toLocaleDateString()
+        }}
+      </h3>
 
-    <h3>{{ tower.capacity }}</h3>
-    <h3>{{ tower.location }}</h3>
-    <div v-if="tower.isCanceled" class="bg-warning">
-      <h4>This is canceled</h4>
+      <h3>{{ tower.capacity }}</h3>
+      <h3>{{ tower.location }}</h3>
+      <div v-if="tower.isCanceled" class="bg-warning">
+        <h4>This is canceled</h4>
+      </div>
+      <div v-if="tower.capacity <= 0" class="bg-danger">
+        <h4>No more seats</h4>
+      </div>
     </div>
-    <div v-if="tower.capacity <= 0" class="bg-danger">
-      <h4>No more seats</h4>
-    </div>
-  </div>
+  </router-link>
 </template>
 
 
