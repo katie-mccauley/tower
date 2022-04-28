ef2e80004c42fbe3ccb9fee43a3cc5397f936c29
Katie McCauley <katiemccau02@gmail.com>
2022-04-28 11:40:49 -0600

    deployment

---
+++ tower/client/assets/AboutPage.00058e90.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.0dadc13f.js";import{o,b as t,d as s}from"./vendor.603ec2c7.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,u,f,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
